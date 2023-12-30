24e530dc52adbc9cb49b291206a6e69cc244269e
RodrigoF <rodrigofernandez338@hotmail.com>
2023-12-30 00:34:55 -0300

    correccion de errores data bases

--- proyecto-main/blog/blog/settings/base.py
+++ proyecto-main/blog/blog/settings/base.py
@@ -17,7 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['micacomision6.pythonanywhere.com']
+ALLOWED_HOSTS = ['rodrigofer.pythonanywhere.com']
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
